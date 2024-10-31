e1e8373443f16dce7ede5c46b175107d980bbbc0
Vraj Patel <vrajpatel9638@gmail.com>
2024-10-31 10:31:05 +0530

    with location - 2

--- app/src/main/java/com/example/monir_try/Location_2.java
+++ app/src/main/java/com/example/monir_try/Location_2.java
@@ -1,6 +1,9 @@
 package com.example.monir_try;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,13 @@ public class Location_2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location2);
 
+        TextView textView = findViewById(R.id.textView28);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Location_2.this, Location_3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
