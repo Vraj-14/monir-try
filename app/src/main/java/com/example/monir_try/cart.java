bd0dcf38d1a8d8d5354167718fd38c1e2adfc624
Vraj Patel <vrajpatel9638@gmail.com>
2024-11-28 11:40:10 +0530

    commit till 28-11-24 11:39

--- app/src/main/java/com/example/monir_try/cart.java
+++ app/src/main/java/com/example/monir_try/cart.java
@@ -1,6 +1,11 @@
 package com.example.monir_try;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +18,38 @@ public class cart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_cart);
+
+        ImageView imageView4 = findViewById(R.id.imageView4);
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart.this, Location_1.class); //
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageview20 = findViewById(R.id.imageView20);
+        imageview20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("HomePage", "Navigating to user_profile...");
+                Intent intent = new Intent(cart.this, com.example.monir_try.user_profile.class);
+                startActivity(intent);
+                Log.d("HomePage", "Intent to user_profile started!");
+            }
+        });
+
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart.this, cart.class); //
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
